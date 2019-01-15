6d7aad8fdbbb461a3744aaa91307030a04d52282
LiZheGuang <1030660726@qq.com>
2019-01-15 19:13:34 +0800

    <type>(<scope>): <subject> (必填项) <BLANK LINE>  //空行 <body>  // 提交内容的详情
    [参考地址](https://shimo.im/docs/ixS7AoISzfo5mQZi/)

--- controller/wechat.js
+++ controller/wechat.js
@@ -9,18 +9,47 @@ const accesstoken = mongoose.model('accesstoken')
 
 // xml2js中间件
 module.exports.xml2js = async (ctx, next) => {
-    console.log('中间件')
+
+    let buffer = [];
+    let that = this
+
     ctx.req.on('data', (chunk) => {
-        // buf += chunk
+
+        buffer.push(chunk);
+
         const parseString = xml2js.parseString
         parseString(chunk, (err, res) => {
             console.log(res)
             ctx.wecahtXmla = res
+            buf = res
         })
     })
     ctx.req.on('end', () => {
         console.log('end')
-        next()
+
+        var msgXml = Buffer.concat(buffer).toString('utf-8');
+
+        const parseString = xml2js.parseString
+
+        //解析xml
+        parseString(msgXml, { explicitArray: false }, function (err, result) {
+            if (!err) {
+                result = result.xml;
+                var toUser = result.ToUserName; //接收方微信
+                var fromUser = result.FromUserName;//发送仿微信
+                console.log(that.txtMsg(fromUser, toUser, '欢迎关注 hvkcoder 公众号，一起斗图吧'))
+                ctx.body = that.txtMsg(fromUser, toUser, '欢迎关注 hvkcoder 公众号，一起斗图吧')
+
+                //判断事件类型
+                // switch (result.Event.toLowerCase()) {
+                //     case 'subscribe':
+                //         //回复消息
+                //         break;
+                // }
+            }
+        })
+        // 
+        // next()
     })
 }
 // xml2jsz转成json
@@ -59,10 +88,49 @@ module.exports.setting = async (query) => {
         return 'mismatch'
     }
 }
-
 module.exports.accessToken = async () => {
     const tokenJson = await wechatClass.wechatToken()
     return tokenJson
+}
+module.exports.createButton = async () => {
+    let createButtonApi = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN'
+    let btnJson = {
+        "button": [
+            {
+                "type": "click",
+                "name": "今日歌曲",
+                "key": "V1001_TODAY_MUSIC"
+            },
+            {
+                "name": "菜单",
+                "sub_button": [
+                    {
+                        "type": "view",
+                        "name": "搜索",
+                        "url": "http://www.soso.com/"
+                    },
+                    {
+                        "type": "miniprogram",
+                        "name": "wxa",
+                        "url": "http://mp.weixin.qq.com",
+                        "appid": "wx286b93c14bbf93aa",
+                        "pagepath": "pages/lunar/index"
+                    },
+                    {
+                        "type": "click",
+                        "name": "赞一下我们",
+                        "key": "V1001_GOOD"
+                    }]
+            }]
+    }
+    const postRes = await axios.post(createButtonApi, btnJson)
+    return postRes.data
+}
+
+// 回复消息
+module.exports.txtMsg = (toUser, fromUser, content) => {
+    var xmlContent = `<xml><ToUserName><![CDATA[${toUser}]]></ToUserName><FromUserName><![CDATA[${fromUser}]]></FromUserName><CreateTime>${new Date().getTime()}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[${content}]]></Content></xml>`
+    return xmlContent;
 }
 
 
@@ -76,7 +144,7 @@ class axiosHttp {
                 resove(res.data)
                 if (res.data.access_token) {
                     setInterval(() => {
-                        this.access_token(res.data.access_token,res.data.expires_in)
+                        this.access_token(res.data.access_token, res.data.expires_in)
                     }, res.data.expires_in * 1000);
                 }
             }).catch((err) => {
@@ -84,7 +152,7 @@ class axiosHttp {
             })
         })
     }
-    async uptoken(access_token,expires_in){
+    async uptoken(access_token, expires_in) {
         accesstoken.findOneAndUpdate({ accessName: "accesstoken" }, {
             $set: {
                 access_token: access_token,
--- router/wechat.js
+++ router/wechat.js
@@ -1,6 +1,7 @@
 const KoaRouter = require('koa-router');
 const wechat = require('../controller/wechat')
 
+const xml2js = require('xml2js');
 
 
 let router = new KoaRouter();
@@ -9,15 +10,72 @@ router.get('/setting', async (ctx, next) => {
     let query = ctx.request.query
     // ctx.body = {code:200}
 
+    console.log('11')
+
     ctx.body = await wechat.setting(query)
     //1.获取微信服务器Get请求的参数 signature、timestamp、nonce、echostr   
 })
 
 
-router.post('/setting', wechat.xml2js, async (ctx, next) => {
-    let wecahtXmla = ctx.wecahtXmla
-    console.log(wecahtXmla.xml.ToUserName)
-    ctx.body = await { ok: true }
+router.post('/setting', async (ctx, next) => {
+    let buffer = [];
+    let that = this
+
+    ctx.res.setHeader('Content-Type', 'application/xml')
+    ctx.req.on('data', (chunk) => {
+
+        buffer.push(chunk);
+
+        const parseString = xml2js.parseString
+        parseString(chunk, (err, res) => {
+            console.log(res)
+            ctx.wecahtXmla = res
+            buf = res
+        })
+    })
+    ctx.req.on('end', () => {
+        console.log('end')
+
+        var msgXml = Buffer.concat(buffer).toString('utf-8');
+
+        const parseString = xml2js.parseString
+        function jsonToXml (obj) {
+            const builder = new xml2js.Builder()
+            return builder.buildObject(obj)
+        }
+        //解析xml
+        parseString(msgXml, { explicitArray: false }, function (err, result) {
+            if (!err) {
+                result = result.xml;
+                var toUser = result.ToUserName; //接收方微信
+                var fromUser = result.FromUserName;//发送仿微信
+                console.log('推送')
+
+                let xml = jsonToXml({
+                    xml: {
+                        ToUserName: toUser,
+                        FromUserName: fromUser,
+                        CreateTime: Date.now(),
+                        MsgType: 'text',
+                        Content: 'content'
+                    }
+                })
+                console.log(xml)
+                ctx.res.end(xml)
+
+                //判断事件类型
+                // switch (result.Event.toLowerCase()) {
+                //     case 'subscribe':
+                //         //回复消息
+                //         break;
+                // }
+            }
+        })
+        // 
+        // next()
+    })
+    // ctx.body = {ok:200}
+    // console.log(wecahtXmla.xml.Event.toLowerCase())
 })
 
 
@@ -26,10 +84,8 @@ router.get('/accessToken', async (ctx, next) => {
 })
 
 
-router.get('/siginTest', async (ctx, next) => {
-  
-
-    ctx.body = {ok:true}
+router.post('/createButton', async (ctx, next) => {
+    ctx.body = await wechat.createButton()
 })
 
 module.exports = router;
